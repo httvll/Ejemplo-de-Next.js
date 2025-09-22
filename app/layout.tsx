319599c40588482b57aa21ae867c08f178e66a48
httvll <hvillagrareyes@gmail.com>
2025-09-21 21:01:35 -0300

    Vinculado a la anterior modificación

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es" className='translated-ltr'>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
